5448b2d5b8be68e582737afe9cf728c50b423974
HashanEranga <45037796+ccmrhe@users.noreply.github.com>
2021-11-14 23:23:34 +0530

    use() : node packet manager npm

---
+++ index.js
@@ -0,0 +1,7 @@
+const superHeroes = require('superheroes')
+let mySuperHero = superHeroes.random()
+console.log(mySuperHero)
+
+const superVillains = require('supervillains')
+let mySuperVillain = superVillains.random()
+console.log(mySuperVillain)
