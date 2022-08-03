c759fdd141e9c7d23ec3f08d778d19693aea2750
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-03 07:03:13 +0530

    parsing url getting query headers payload, method

--- nodejsBasics/routing requests/server.js
+++ nodejsBasics/routing requests/server.js
@@ -1,24 +1,45 @@
 var http = require('http')
+var url = require('url')
+var StringDecoder = require('string_decoder').StringDecoder
 
 http.createServer((req,res)=>{
-    const url = req.url
-    if (url === '/'){
+    // parsed url
+    var parsedUrl = url.parse(req.url, true)
+
+    // path 
+    var path = parsedUrl.pathname
+
+    // trimmed_path
+    var trimmed_path = path.replace(/^\/+|\/+$/g,'')
+    
+    // http method
+    var method = req.method
+
+    // http query 
+    var query = parsedUrl.query
+
+    // headers
+    var headers = req.headers
+
+    // payload
+    var decoder = new StringDecoder('utf-8')
+    var buffer = ''
+
+    req.on('data', (data)=>{
+        buffer += decoder.write(data)
+    })
+
+    req.on('end', ()=>{
+        buffer += decoder.end()
         res.setHeader('content-type','text/html')
         res.write('<html>')
         res.write('<head><title>Server Responce</title></head>')
-        res.write('<body><h1>Form</h1>')
-        res.write('<form action="/message" method="POST"><input type="text" name="message">')
-        res.write('<button type="submit">Submit</button></form>')
-        res.write('</body>')
+        res.write('<body><h1>This is the server responce</h1></body>')
         res.write('</html>')
-        return res.end()
-    }
-    res.setHeader('content-type','text/html')
-    res.write('<html>')
-    res.write('<head><title>Server Responce</title></head>')
-    res.write('<body><h1>This is the server responce</h1></body>')
-    res.write('</html>')
-    res.end()
+        res.end()
+        console.log('Headers : ', headers, ' Method : ', method, ' Query : ', query, ' Path : ', trimmed_path, ' Payload : ', buffer)
+    })
+    
 
 }).listen(3000, ()=>{
     console.log('Server is listening to port : ', 3000)
