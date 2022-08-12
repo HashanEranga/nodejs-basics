d7b010d31b6e880a79fde1688c719ff467390dc8
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-12 15:42:15 +0530

    create routes

--- nodejsBasics/Server using ExpressJS/server.js
+++ nodejsBasics/Server using ExpressJS/server.js
@@ -2,12 +2,12 @@ var express = require('express')
 
 var app = express()
 
-app.use((req, res, next)=>{
+app.use('/add-products', (req, res, next)=>{
     console.log("in the middleware")
     // function that pass on to another middle ware
-    next()
+    res.send('<h1>Hello from add products</h1>')
 })
-app.use((req, res, next)=>{
+app.use('/', (req, res, next)=>{
     console.log("in another middleware")
     res.send('<h1>Hello from expressjs</h1>')
 })
