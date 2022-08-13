725a0c117d7fa63e577306ced399b1b7ad77ddba
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-14 05:06:13 +0530

    create a page not found using middleware

--- nodejsBasics/Server using ExpressJS/server.js
+++ nodejsBasics/Server using ExpressJS/server.js
@@ -2,12 +2,14 @@ var express = require('express')
 var bodyParser = require('body-parser')
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+const notFoundRoute = require('./routes/notFound')
 
 var app = express()
 app.use(bodyParser.urlencoded({extended:false}))
-app.use(shopRoutes)
-app.use(adminRoutes)
 
+app.use(adminRoutes)
+app.use(shopRoutes)
+app.use(notFoundRoute)
 
 app.listen(3000, ()=>{
     console.log('Server is listening on port : ', 3000)
