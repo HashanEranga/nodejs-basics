f889a96e74837b381c3c57a03698589161e95f07
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-14 04:59:50 +0530

    using express Router

--- nodejsBasics/Server using ExpressJS/server.js
+++ nodejsBasics/Server using ExpressJS/server.js
@@ -1,19 +1,14 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
 
 var app = express()
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(shopRoutes)
+app.use(adminRoutes)
 
 
-app.get('/add-products', (req, res, next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
+app.listen(3000, ()=>{
+    console.log('Server is listening on port : ', 3000)
 })
-app.post('/product', (req, res, next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-app.use('/', (req, res, next)=>{
-    res.send('<h1>Hello from expressjs</h1>')
-})
-
-app.listen(3000)
