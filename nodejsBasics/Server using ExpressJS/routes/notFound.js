cd2ddacb5495f436ac0c5c7b0b4d928ea7ae89e0
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-14 12:03:07 +0530

    use a helper to determine paths

--- nodejsBasics/Server using ExpressJS/routes/notFound.js
+++ nodejsBasics/Server using ExpressJS/routes/notFound.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const path = require('path')
-const router = express.Router()
+const rootDir = require('../utils/path')
 
+const router = express.Router()
 router.use((req,res,next)=>{
-    res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'))
+    res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
 })
 
 module.exports = router
