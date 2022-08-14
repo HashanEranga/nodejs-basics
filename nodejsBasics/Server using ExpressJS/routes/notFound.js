c13442c8c68152ebdf33bf92b8cc67ca7539f2e5
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2022-08-14 10:19:37 +0530

    create not found page return html files

--- nodejsBasics/Server using ExpressJS/routes/notFound.js
+++ nodejsBasics/Server using ExpressJS/routes/notFound.js
@@ -1,9 +1,9 @@
 const express = require('express')
-
+const path = require('path')
 const router = express.Router()
 
 router.use((req,res,next)=>{
-    res.status(404).send('<h1>Page Not Found</h1>')
+    res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'))
 })
 
 module.exports = router
